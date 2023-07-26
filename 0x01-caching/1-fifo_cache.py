d90c4cfb610bb8ef3fa66f6d719bb18b80f39f37
Dennis Murage <dmurage140@gmail.com>
2023-07-26 14:41:47 +0300

    Added task 0-4 files implementing different caching systems

---
+++ 0x01-caching/1-fifo_cache.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+"""class to add elements to the cache"""
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class FIFOCache(BaseCaching):
+    def __init__(self):
+        """_summary_
+        """
+        super().__init__()
+
+    def put(self, key, item):
+        """_summary_
+
+        Args:
+            key (_type_): _description_
+            item (_type_): _description_
+        """
+        if key and item:
+            self.cache_data[key] = item
+            if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+                key_discard = next(iter(self.cache_data))
+                self.cache_data.pop(key_discard)
+                print("DISCARD {}".format(key_discard))
+
+    def get(self, key):
+        """_summary_
+
+        Args:
+            key (_type_): _description_
+
+        Returns:
+            _type_: _description_
+        """
+        return self.cache_data.get(key)
---
+++ 0x01-caching/2-lifo_cache.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+"""class to add elements to the cache"""
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class LIFOCache(BaseCaching):
+    def __init__(self):
+        """_summary_
+        """
+        super().__init__()
+
+    def put(self, key, item):
+        """_summary_
+
+        Args:
+            key (_type_): _description_
+            item (_type_): _description_
+        """
+        if key and item:
+            if len(self.cache_data) >= BaseCaching.MAX_ITEMS and self.cache_data.get(key) == None:
+                k, v = self.cache_data.popitem()
+                print("DISCARD {}".format(k))
+            self.cache_data[key] = item
+
+
+    def get(self, key):
+        """_summary_
+
+        Args:
+            key (_type_): _description_
+
+        Returns:
+            _type_: _description_
+        """
+        return self.cache_data.get(key)
