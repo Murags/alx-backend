190dad1dd76941c2005ff5a146f469e2f6a0c759
Dennis Murage <dmurage140@gmail.com>
2023-08-05 13:41:25 +0300

    Added task 0 files and README.md

---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+"""_summary_
+
+    Returns:
+        _type_: _description_
+"""
+
+from flask import Flask, render_template
+from flask_babel import Babel
+
+
+app = Flask(__name__)
+
+babel = Babel(app)
+
+class Config(object):
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'fr'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+app.config.from_object(Config)
+
+@app.route('/')
+def home():
+    return render_template("1-index.html")
+
+app.run(debug=True)
---
+++ 0x02-i18n/2-app.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+from flask import Flask, render_template
+from flask_babel import Babel
+
+
+app = Flask(__name__)
+
+babel = Babel(app)
+
+class Config(object):
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'fr'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+app.config.from_object(Config)
+
+@app.route('/')
+def home():
+    return render_template("1-index.html")
+
+app.run(debug=True)
