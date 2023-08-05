26287f3f9792d051496a6a271c66e005c0a44a74
Dennis Murage <dmurage140@gmail.com>
2023-08-05 13:58:45 +0300

    Fixing Styling

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -4,8 +4,13 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route("/")
-def home():
+@app.route("/", strict_slashes=False)
+def home() -> str:
+    """_summary_
+
+    Returns:
+        _type_: _description_
+    """
     return render_template('index.html')
 
 app.run()
