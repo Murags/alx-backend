1e5456003f971b0a799a4501a569b1f4e2e2c6c7
Dennis Murage <dmurage140@gmail.com>
2023-08-06 12:02:05 +0300

    Error Handle

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -12,7 +12,8 @@ def home() -> str:
     Returns:
         _type_: _description_
     """
-    return render_template('index.html')
+    return render_template('0-index.html')
+
 
 if __name__ == "__main__":
     app.run()
